b2d4292b0a6939c81f495460d0b24ccfd91f39f3
qxdn <1464238196@qq.com>
2019-12-28 13:04:41 +0800

    ipram

--- counter.v
+++ counter.v
@@ -1,27 +1,16 @@
-module counter(CLK,RESET_N,EN,LOAD_N,DATA,CNT,COUT);
-input CLK,RESET_N,EN,LOAD_N;
-input [3:0] DATA;
-output reg [3:0] CNT;
-output reg COUT;
+module counter(CLK,EN,LOAD_N,DATA,CNT);
+input CLK,EN,LOAD_N;
+input [7:0] DATA;
+output reg [7:0] CNT;
 
-always @(posedge CLK or negedge RESET_N) begin
-    if (!RESET_N) begin
-        CNT<=4'd0;  //复位
-        COUT<=1'b0;
-    end else if(EN) begin //使能
-        if (!LOAD_N) begin
-            CNT<=DATA;
-        end else if(CNT<4'd7) begin
-            CNT<=CNT+1'b1;
-            COUT<=1'b0;
-        end else begin
-            CNT<=4'd0;
-            COUT<=1'b1;
-        end
+//异步置位 同步复位
+always @(posedge CLK or negedge LOAD_N) begin
+    if (!LOAD_N) begin
+        CNT<=DATA;  
+    end else if(EN) begin
+        CNT<=CNT+1'b1;
     end
 end
 
-
-
-
-endmodule  
+endmodule
+  
